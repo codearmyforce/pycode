8d651d6ebb7a7f9eab2e9667235399b075160f7d
Ravi Kumar <erravicsekumar@gmail.com>
2021-06-22 06:05:19 +0000

    first update

--- ex1.py
+++ ex1.py
@@ -1,1 +1,8 @@
-print("Hello world")
+class student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+    def disp(self):
+        print("Hello my name is {} and roll is {}.".format(self.name, self.roll))
+ob=student("Ravi", 120)
+ob.disp()
