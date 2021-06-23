ad03edd025b08d002523966fef2eda139549148a
Ravi Kumar <erravicsekumar@gmail.com>
2021-06-23 03:07:38 +0000

    This is second Execricse

---
+++ ex2.py
@@ -0,0 +1,9 @@
+'''
+Write a program that asks the user to enter his or her name. The program should
+respond with a message that says hello to the user, using his or her name.
+'''
+class test:
+    if __name__ == "__main__":
+        name=raw_input("Enter your first name :")
+        print("Hello {}".format(name))
+
